b5bf450850578279d72d7ca1ac8d20394008841e
Anthony <anthony.tarlao@gmail.com>
2016-08-16 23:38:09 +0200

    API : route get pages + response JSON

--- src/AppBundle/Controller/ApiController.php
+++ src/AppBundle/Controller/ApiController.php
@@ -3,9 +3,12 @@
 namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+use AppBundle\Entity\Page;
 
 /**
  * @Route("/api")
@@ -15,10 +18,14 @@ class ApiController extends Controller
   /**
    * @Route("/get/pages/{id}", name="api_get_page")
    * @Method({"GET"})
-   * @ParamConverter("page", class="AppBundle:Page")
    */
   public function indexAction(Page $page)
   {
-      return $this->render('default/index.html.twig');
+      $response = new JsonResponse();
+      $response->setData(array(
+        'data' => $page->getContent()
+      ));
+
+      return $response;
   }
 }
